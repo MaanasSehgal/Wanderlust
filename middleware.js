46b1bc374b7357339dbe2b1202ba946e6ca0d739
Maanas Sehgal <maanas.sehgal@gmail.com>
2024-01-13 23:08:22 +0530

    changes made in user.js and middleware.js

--- middleware.js
+++ middleware.js
@@ -1,7 +1,16 @@
 module.exports.isLoggedIn = (req, res, next) => {
+	// console.log(req.path, "..", req.originalUrl)
 	if (!req.isAuthenticated()) {
+		req.session.redirectUrl = req.originalUrl;
 		req.flash("error", "You must be logged in to create a listing!");
 		return res.redirect("/login");
 	}
-    next();
+	next();
+};
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+	if (req.session.redirectUrl) {
+		res.locals.redirectUrl = req.session.redirectUrl;
+	}
+	next();
 };
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const passport = require("passport"); // Make sure you have this line
 const User = require("../models/user");
 const wrapAsync = require("../utils/wrapAsync");
+const { saveRedirectUrl } = require("../middleware");
 
 router.get("/signup", (req, res) => {
 	res.render("users/signup.ejs");
@@ -16,8 +17,13 @@ router.post(
 			const newUser = new User({ email, username });
 			const registeredUser = await User.register(newUser, password);
 			// console.log(registeredUser);
-			req.flash("success", "Welcome to Wanderlust!");
-			res.redirect("/listings");
+			req.login(registeredUser, (err) => {
+				if (err) {
+					return next(err);
+				}
+				req.flash("success", "Welcome to Wanderlust!");
+				res.redirect("/listings");
+			});
 		} catch (err) {
 			req.flash("error", err.message);
 			res.redirect("/signup");
@@ -31,10 +37,13 @@ router.get("/login", (req, res) => {
 
 router.post(
 	"/login",
+	saveRedirectUrl,
 	passport.authenticate("local", { failureRedirect: "/login", failureFlash: true }),
 	wrapAsync(async (req, res) => {
 		req.flash("success", "Welcome to Wanderlust! You are logged in!");
-		res.redirect("/listings");
+		//to redirect user to last page they were on
+		let redirectUrl = res.locals.redirectUrl || "/listings";
+		res.redirect(redirectUrl);
 	}),
 );
 
